16003915c8f761f43c0b3c18b54a60417086d426
Igal Shnaider <igalshnaider@Igals-MacBook-Pro.local>
2016-06-15 17:32:17 +0300

    Added Handler

--- wiki.go
+++ wiki.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 //Page structer
@@ -25,9 +26,16 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// func main() {
+// 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+// 	p1.save()
+// 	p2, _ := loadPage("TestPage")
+// 	fmt.Println(string(p2.Body))
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
