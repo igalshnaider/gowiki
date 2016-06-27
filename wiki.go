acd5f0247bea7a5cd12ad4ef9d43a8c1825415eb
Igal Shnaider <igalshnaider@Igals-MacBook-Pro.local>
2016-06-27 19:39:49 +0300

    adding futures

--- wiki.go
+++ wiki.go
@@ -12,7 +12,9 @@ import (
 //Global
 //Executes with the initializating of program
 //If specified .html files does not exist than PANIC
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var tmplpath = "tmpl/"
+var datapath = "data/"
+var templates = template.Must(template.ParseFiles(tmplpath+"edit.html", tmplpath+"view.html"))
 
 //Global
 //MustCompile is distinct from Compile in that it will panic if the expression compilation fails,
@@ -26,12 +28,14 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := datapath + p.Title + ".txt"
+	//filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := datapath + title + ".txt"
+	//filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -64,11 +68,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 		return
 	}
 }
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+
+//View Daf in hebrew problem
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -77,11 +80,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -89,23 +89,33 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err = p.save()
+	err := p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
 	http.ListenAndServe(":8080", nil)
 }
+
+//https://portswigger.net/knowledgebase/papers/
